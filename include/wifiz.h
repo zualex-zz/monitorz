20de5cc2f2b2903c0328565a1567ba5357a3d056
Alex Zupan <alex.zupan@gpi.it>
2024-10-07 11:44:37 +0200

    fix ota

--- include/wifiz.h
+++ include/wifiz.h
@@ -5,27 +5,32 @@
 #include <ESPmDNS.h>
 #include <ArduinoOTA.h>
 
-class Wifiz {
-    const char * ssid;
-    const char * password;
+class Wifiz
+{
+    const char *ssid;
+    const char *password;
+
 public:
     bool connected = false;
 
+    Wifiz(const char *ssid, const char *password) : ssid(ssid), password(password) {}
 
-    Wifiz(const char * ssid, const char * password) : ssid(ssid), password(password) {}
-
-    void begin() {
+    void begin()
+    {
         Serial.println("WiFi begin");
         WiFi.begin(ssid, password);
     }
 
-    bool justConnected() {
-        if (this->connected == false && WiFi.isConnected()) {
+    bool justConnected()
+    {
+        if (this->connected == false && WiFi.isConnected())
+        {
             this->connected = true;
             Serial.print("WiFi IP address: ");
             Serial.println(WiFi.localIP());
 
-            if (MDNS.begin("cam")) {
+            if (MDNS.begin("cam"))
+            {
                 MDNS.addService("http", "tcp", 80);
             }
 
@@ -35,8 +40,8 @@ public:
         return false;
     }
 
-    
-    void enableOTA() {
+    void enableOTA()
+    {
 
         // if (!connected) {
         //     Serial.println("Not connected to wifi.. ");
@@ -56,7 +61,8 @@ public:
         // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
         // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-        ArduinoOTA.onStart([]() {
+        ArduinoOTA.onStart([]()
+                           {
             String type;
             if (ArduinoOTA.getCommand() == U_FLASH)
                 type = "sketch";
@@ -64,31 +70,31 @@ public:
                 type = "filesystem";
 
             // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-            Serial.println("Start updating " + type);
-        })
-        .onEnd([]() {
-            Serial.println("\nEnd");
-        })
-        .onProgress([](unsigned int progress, unsigned int total) {
-            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-        })
-        .onError([](ota_error_t error) {
+            Serial.println("Start updating " + type); })
+            .onEnd([]()
+                   { Serial.println("\nEnd"); })
+            .onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+            .onError([](ota_error_t error)
+                     {
             Serial.printf("Error[%u]: ", error);
             if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
             else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
             else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
             else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-            else if (error == OTA_END_ERROR) Serial.println("End Failed");
-        });
+            else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
 
         ArduinoOTA.begin();
         Serial.println("OTA Started");
     }
 
-    void handleOTA() {
-        ArduinoOTA.handle();
+    void handleOTA()
+    {
+        if (this->connected)
+        {
+            ArduinoOTA.handle();
+        }
     }
-
 };
 
 #endif
